491b1ebb463bff35b80b0c0ee01225094784b207
gurpreetsingh9465 <amansingh9569@gmail.com>
2019-11-14 20:34:06 +0530

    heroku

--- aqi/wsgi.py
+++ aqi/wsgi.py
@@ -10,7 +10,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feed.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aqi.settings')
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
